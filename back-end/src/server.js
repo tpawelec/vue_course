d81fffb70e4a885e76d09a66a76bc293abe490d7
Tomasz Pawelec <tpawelec@outlook.com>
2025-08-10 13:31:11 +0200

    backend, express, mongodb (locally)

---
+++ back-end/src/server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import { cartItems as cartItemsRaw, products as productsRaw} from './temp-data.js';
+
+let cartItems = cartItemsRaw
+let products = productsRaw
+
+const app = express();
+
+function populateCartIds(ids) {
+    return ids.map(id => products.find(product => product.id === id))
+}
+
+app.use(express.json())
+
+app.get('/hello', (req, res) => {
+    res.send("Hello kurwa")
+})
+
+app.get('/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/cart', (req, res) => {
+    const populatedCart = populateCartIds(cartItems)
+    res.json(populatedCart)
+})
+
+app.get('/products/:productId', (req, res) => {
+    const productId = req.params.productId
+    const product = products.find(product => product.id === productId)
+    res.json(product)
+})
+
+app.post('/cart', (req, res) => {
+    const productId = req.body.id
+    cartItems.push(productId)
+    const populatedCart = populateCartIds(cartItems)
+    res.json(populatedCart);
+})
+
+app.delete('/cart/:productId', (req, res) => {
+    const productId = req.params.productId
+    cartItems = cartItems.filter(id => id !== productId)
+    const populatedCart = populateCartIds(cartItems)
+    res.json(populatedCart);
+
+})
+app.listen(8000, () => {
+    console.log('Server is running on 8000')
+})
